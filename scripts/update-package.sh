1ec17b0a73c0b3c1fac08ee5a1c45f549a18eb61
mcchrish <mcchrish@gmail.com>
2015-10-08 17:24:35 +0800

    Update vim also

--- scripts/update-package.sh
+++ scripts/update-package.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "###############################"
-echo "Updating" + `date`
+echo "Updating" `date`
 brew update && brew upgrade
-cask update
+brew cask update
 npm update -g
+vim +PlugUpdate +PlugUpgrade +qall!
 echo "Done"
 echo "###############################"
