3cccc81d2a99eae886abc3257c6ee7b4b2e83066
mcchrish <mcchrish@gmail.com>
2015-11-30 18:29:37 +0800

    Export karabiner settings

---
+++ karabiner/karabiner-export.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.emacsmode_ex_controlW 1
+/bin/echo -n .
+$cli set repeat.initial_wait 500
+/bin/echo -n .
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set option.emacsmode_optionV 1
+/bin/echo -n .
+$cli set option.emacsmode_optionD 1
+/bin/echo -n .
+$cli set remap.hjkl_arrow 1
+/bin/echo -n .
+$cli set option.emacsmode_optionBF 1
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+$cli set option.emacsmode_controlAE 1
+/bin/echo -n .
+/bin/echo
