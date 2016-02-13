3c867b897cdee834b32710dee0da962fe7068f7b
mcchrish <mcchrish@gmail.com>
2016-02-13 10:39:57 +0800

    zsh: cd into current Finder directory

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -80,3 +80,10 @@ fcoc() {
   commit=$(echo "$commits" | fzf --tac +s +m -e) &&
   git checkout $(echo "$commit" | sed "s/ .*//")
 }
+
+# cd into the current Finder directory
+cdf() {
+  local dir
+  dir=$(osascript -e 'tell application "Finder" to get the POSIX path of (target of front window as alias)' 2> /dev/null) \
+    && cd "$dir"
+}
