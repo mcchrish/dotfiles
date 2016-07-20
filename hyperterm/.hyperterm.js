bb1cdb0d7bfc8e64a748833d5d238668a394c917
mcchrish <mcchrish@gmail.com>
2016-07-20 09:52:37 +0800

    add hyperterm.js

---
+++ hyperterm/.hyperterm.js
@@ -0,0 +1,49 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 12,
+
+    // font family with optional fallbacks
+    fontFamily: '"FiraCode-Retina", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    // terminal cursor background color (hex)
+    cursorColor: '',
+
+    // color of the text
+    foregroundColor: '',
+
+    // terminal background color
+    backgroundColor: '',
+
+    // border color (window, tabs)
+    borderColor: '',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    termCSS: '',
+
+    // custom padding
+    padding: '4px 4px',
+
+    // some color overrides. see http://bit.ly/29k1iU2 for
+    // the full list
+    colors: []
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hypersolar`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+    'hyperterm-gruvbox-dark'
+  ],
+
+  // in development, you can create a directory under
+  // `~/.hyperterm_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+}
