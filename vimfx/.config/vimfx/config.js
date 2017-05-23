36a34d30b1218edce14042536fc83fad94fb8779
mcchrish <mcchrish@gmail.com>
2017-05-23 10:41:31 +0800

    vimfx: updates

--- vimfx/.config/vimfx/config.js
+++ vimfx/.config/vimfx/config.js
@@ -1,8 +1,10 @@
 const options = {
+  "mode.normal.paste_and_go": "",
+  "mode.normal.paste_and_go_in_tab": "",
   "mode.normal.scroll_half_page_down": "d <c-d>",
   "mode.normal.scroll_half_page_up": "u <c-u>",
-  "mode.normal.tab_select_previous": "J    gT <c-h>",
-  "mode.normal.tab_select_next": "K    gt <c-l>",
+  "mode.normal.tab_select_previous": "J    gT <c-h> <c-j>",
+  "mode.normal.tab_select_next": "K    gt <c-l> <c-k>",
   "mode.normal.esc": "<force><escape> <force><c-c>",
   "mode.hints.exit": "<escape> <c-c>",
   "mode.ignore.exit": "<s-escape> <force><c-c>",
