55a75cf45e5d5013a799bafd08d380a853de6d84
Michael Chris Lopez <hello@michaelchris.space>
2018-12-11 18:18:54 +0800

    install: setup XDG directories
    Just in case

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# XDG
+if [[ -z "$XDG_CONFIG_HOME" ]]; then
+  export XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+if [[ -z "$XDG_CACHE_HOME" ]]; then
+  export XDG_CACHE_HOME="$HOME/.cache"
+fi
+
+if [[ -z "$XDG_DATA_HOME" ]]; then
+  export XDG_DATA_HOME="$HOME/.local/share"
+fi
+
+if [[ -z "$XDG_DATA_DIRS" ]]; then
+  export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+fi
+
+if [[ -z "$XDG_CONFIG_DIRS" ]]; then
+  export XDG_CONFIG_DIRS="/etc/xdg"
+else
+  export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_DIRS"
+fi
+
 cd "$HOME" || exit
 
 git clone https://github.com/mcchrish/dotfiles
