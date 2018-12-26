ba040e078fe8caf76b7046ba115cfad49fff0a34
Michael Chris Lopez <hello@michaelchris.space>
2018-12-26 08:21:35 +0800

    brew: pass npm cache location when installing

--- stash/brewfile.sh
+++ stash/brewfile.sh
@@ -50,6 +50,10 @@ brew cask install visual-studio-code
 brew cask install vlc
 
 # npm
+export npm_config_userconfig="$XDG_CONFIG_HOME/npm/config"
+export npm_config_cache="$XDG_CACHE_HOME/npm"
+export npm_config_prefix="$XDG_DATA_HOME/npm"
+export npm_config_init_module="$XDG_DATA_HOME/npm/config/npm-init.js"
 npm install -g trash-cli npm-check
 
 # pip
