61223b8cf6c1ffa713677ee0e7ea04a470ab65de
Michael Chris Lopez <hello@michaelchris.space>
2018-12-28 19:04:01 +0800

    install: fix install brew

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ cd .dotfiles || exit
 
 if ! (command -v brew > /dev/null); then
   echo "Installing brew..."
-  /user/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "Install all programs..."
