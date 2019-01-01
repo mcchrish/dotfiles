9c82db537ce45df3915580c1377d7168f1257deb
Michael Chris Lopez <hello@michaelchris.space>
2019-01-01 13:02:58 +0800

    brew: cleanup packages

--- stash/brewfile.sh
+++ stash/brewfile.sh
@@ -7,14 +7,13 @@ brew install rust
 brew install python
 brew install yarn
 brew install bat
-brew install diff-so-fancy --HEAD
-brew install exa --HEAD
+brew install diff-so-fancy
 brew install fd
 brew install fzf
 brew install git-extras
 brew install gnupg
-brew install neovim --HEAD
-brew install nnn --HEAD
+brew install neovim
+brew install nnn
 brew install pass
 brew install ripgrep
 brew install stow
@@ -23,7 +22,6 @@ brew install tmux
 
 # cask
 brew cask install aerial
-brew cask install alfred
 brew cask install android-file-transfer
 brew cask install calibre
 brew cask install dash
@@ -32,22 +30,22 @@ brew cask install font-courier-prime
 brew cask install font-fira-sans
 brew cask install font-iosevka
 brew cask install google-chrome
-brew cask install gpgtools
 brew cask install iina
 brew cask install iterm2
 brew cask install java
 brew cask install keepingyouawake
 brew cask install keybase
 brew cask install marked
+brew cask install nightowl
+brew cask install pinentry-mac
 brew cask install postman
 brew cask install qtpass
 brew cask install signal
 brew cask install skype
 brew cask install spotify
 brew cask install spotify-notifications
+brew cask install sublime-text
 brew cask install qbittorrent
-brew cask install visual-studio-code
-brew cask install vlc
 
 # npm
 export npm_config_userconfig="$XDG_CONFIG_HOME/npm/config"
@@ -57,7 +55,6 @@ export npm_config_init_module="$XDG_DATA_HOME/npm/config/npm-init.js"
 npm install -g trash-cli npm-check
 
 # pip
-pip2 install pynvim
 pip3 install pynvim
 pip3 install --user neovim-remote
 pip3 install --user tmuxp
