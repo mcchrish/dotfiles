3aeb16f086091d9a375875b5dc168b23f93c8339
Michael Chris Lopez <hello@michaelchris.space>
2019-01-13 09:16:39 +0800

    general: update npm and zsh plugins

--- scripts/update-package.sh
+++ scripts/update-package.sh
@@ -10,13 +10,17 @@ printf "%s\\n" "Updating brew..."
 brew update && brew upgrade
 
 printf "%s\\n" "Updating npm..."
-npm update -g
+npm-check --update-all --global
 
 printf "%s\\n" "Updating pip..."
 pip3 freeze — local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
 
 printf "%s\\n" "Updating neovim..."
 nvim --headless +PlugUpdate +PlugUpgrade +UpdateRemotePlugins +qall
+
+printf "%s\\n" "Updating zsh..."
+zplg update --all
+zplg self-update
 
 printf "%s\\n" "Cleaning up"
 brew cleanup
