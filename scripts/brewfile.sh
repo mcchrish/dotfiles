f3d848bf1ae5714f850d5b3dc8c7d53ea0510c63
Michael Chris Lopez <hello@michaelchris.space>
2020-05-05 16:26:26 +0800

    brew: update some install

--- scripts/brewfile.sh
+++ scripts/brewfile.sh
@@ -5,8 +5,6 @@ brew tap homebrew/cask-fonts
 
 # brew
 brew install node
-brew install python
-brew install cocoapods
 brew install fd
 brew install fzf
 brew install gnupg
@@ -17,12 +15,10 @@ brew install rbenv
 brew install ripgrep
 brew install stow
 brew install tmux
-brew install tmuxinator
 
 # cask
 brew cask install calibre
 brew cask install firefox
-brew cask install fsnotes
 brew cask install font-courier-prime
 brew cask install font-iosevka
 brew cask install font-iosevka-slab
@@ -44,7 +40,17 @@ export npm_config_userconfig="$XDG_CONFIG_HOME/npm/config"
 export npm_config_cache="$XDG_CACHE_HOME/npm"
 export npm_config_prefix="$XDG_DATA_HOME/npm"
 export npm_config_init_module="$XDG_DATA_HOME/npm/config/npm-init.js"
-npm install -g trash-cli npm-check
+npm install -g trash-cli
+npm install -g npm-check
+
+# gem
+rbenv init
+rbenv install 2.7.1
+rbenv global 2.7.1
+rbenv shell 2.7.1
+
+gem install tmuxinator
+gem install cocoapods
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
