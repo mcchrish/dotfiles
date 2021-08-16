3d7d912185a4e604c02f719cc588607dc300d8c6
Michael Chris Lopez <hello@michaelchris.space>
2021-08-16 17:19:04 +0800

    comment empty

--- vim/Library/Preferences/nvim/lua/vendor/comment.lua
+++ vim/Library/Preferences/nvim/lua/vendor/comment.lua
@@ -1,6 +1,5 @@
 return function()
 	require("nvim_comment").setup {
-		comment_empty = false,
 		hook = function()
 			if vim.api.nvim_buf_get_option(0, "filetype") == "vue" then
 				require("ts_context_commentstring.internal").update_commentstring()
