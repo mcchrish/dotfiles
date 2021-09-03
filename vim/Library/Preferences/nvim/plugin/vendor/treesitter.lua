b193e77d141680afc58c9f9ba573a0d523bc37b2
Michael Chris Lopez <hello@michaelchris.space>
2021-09-03 19:08:10 +0800

    incremental_selection treesitter

--- vim/Library/Preferences/nvim/plugin/vendor/treesitter.lua
+++ vim/Library/Preferences/nvim/plugin/vendor/treesitter.lua
@@ -23,4 +23,13 @@ require("nvim-treesitter.configs").setup {
 	indent = {
 		enable = true,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 }
