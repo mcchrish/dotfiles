72339afaa30db107d157e6702352180b44e635d6
Michael Chris Lopez <hello@michaelchris.space>
2021-09-20 17:15:57 +0800

    coq icons

--- vim/Library/Preferences/nvim/plugin/vendor/opt.lua
+++ vim/Library/Preferences/nvim/plugin/vendor/opt.lua
@@ -47,8 +47,139 @@ g.coq_settings = {
 			fast_close = false,
 		},
 		icons = {
-			mode = "none",
+			-- mode = "none",
+			mode = "short",
+			alias = {
+				Enum = "Value",
+				EnumMember = "Value",
+				Method = "Constructor",
+			},
+			mappings = {
+				Class = "⬢",
+				Color = "❁",
+				Constant = "π",
+				Constructor = "⬡",
+				Event = "⤸",
+				Field = "◧",
+				File = "∎",
+				Folder = "⋃",
+				Function = "λ",
+				Interface = "⊷",
+				Keyword = "𝑘",
+				Module = "■",
+				Operator = "÷",
+				Property = "⋯",
+				Reference = "→",
+				Snippet = "◸",
+				Struct = "▣",
+				Text = "ᴀ",
+				TypeParameter = "ᴛ",
+				Unit = "␣",
+				Value = "⧉",
+				Variable = "𝑣",
+			},
 		},
+	},
+}
+
+require("gitsigns").setup {
+	signs = {
+		add = { text = "┃" },
+		change = { text = "┃" },
+		delete = { text = "━" },
+		topdelete = { text = "━" },
+		changedelete = { text = "┳" },
+	},
+}
+
+require("nnn").setup {
+	command = "nnn -oCQ",
+	set_default_mappings = 0,
+	replace_netrw = 1,
+	action = {
+		["<c-t>"] = "tab split",
+		["<c-s>"] = "split",
+		["<c-v>"] = "vsplit",
+		["<c-o>"] = function(lines)
+			local joined_lines = table.concat(lines, "\n")
+			vim.fn.setreg("+", joined_lines)
+		end,
+		["<c-w>"] = function(lines)
+			local dir = lines[#lines]
+			if vim.fn.filereadable(dir) then
+				dir = vim.fn.fnamemodify(dir, ":h")
+			end
+			vim.api.nvim_command("cd " .. dir)
+		end,
+	},
+}
+
+require("nvim_comment").setup {
+	hook = function()
+		if vim.api.nvim_buf_get_option(0, "filetype") == "vue" then
+			require("ts_context_commentstring.internal").update_commentstring()
+		end
+	end,
+}
+
+g.indent_blankline_filetype = { "yaml", "toml", "python" }
+g.indent_blankline_show_first_indent_level = false
+g.indent_blankline_use_treesitter = true
+
+require("fzf-lua").setup {
+	winopts = {
+		win_width = 0.9,
+		win_height = 0.6,
+		win_row = 0.50,
+		win_col = 0.50,
+	},
+	fzf_layout = "default",
+	fzf_info = "--info=default",
+	fzf_binds = {
+		"alt-a:select-all",
+		"alt-d:deselect-all",
+		"down:half-page-down",
+		"up:half-page-up",
+		"shift-down:preview-page-down",
+		"shift-up:preview-page-up",
+	},
+	preview_horizontal = "right:50%",
+	previewers = {
+		builtin = {
+			syntax = false,
+		},
+	},
+	files = {
+		file_icons = false,
+		git_icons = false,
+	},
+	git = {
+		files = {
+			file_icons = false,
+		},
+		status = {
+			file_icons = false,
+		},
+	},
+	grep = {
+		file_icons = false,
+		git_icons = false,
+		rg_opts = "--column --line-number --no-heading --color=always --smart-case --colors='path:fg:white' --colors='path:style:intense'",
+	},
+	buffers = {
+		file_icons = false,
+		git_icons = false,
+	},
+	blines = {
+		previewer = false,
+	},
+	quickfix = {
+		file_icons = false,
+		git_icons = false,
+	},
+	lsp = {
+		file_icons = false,
+		git_icons = false,
 	},
 }
 
