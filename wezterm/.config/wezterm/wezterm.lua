50f4ec09afe5ab98e9044acb441fa9fb931099d5
Michael Chris Lopez <hello@michaelchris.space>
2023-06-18 18:47:34 +0800

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,18 @@
 local wezterm = require "wezterm"
 local act = wezterm.action
 
-local config = {}
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 config.color_scheme = "zenbones"
-config.font = wezterm.font "Iosevka Curly Slab"
+-- https://github.com/wez/wezterm/issues/3616 font looks too thin
+config.font = wezterm.font("Iosevka Curly Slab", { weight = "Medium" })
 config.font_size = 14
 config.initial_cols = 160
 config.initial_rows = 40
+config.front_end = "WebGpu"
+-- config.automatically_reload_config = false
+-- config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
 
 config.keys = {
 	{
@@ -39,6 +41,21 @@ config.unix_domains = {
 	{
 		name = "fo",
 	},
+}
+
+wezterm.on("gui-startup", function()
+	require("workspaces").setup()
+end)
+
+config.window_frame = {
+	font = wezterm.font { family = "Iosevka Curly Slab", weight = "Bold" },
+}
+
+config.window_padding = {
+	left = 8,
+	right = 8,
+	top = 0,
+	bottom = 0,
 }
 
 return config
