3db8985a089b969bf22d5859e1a2aec532c2dbc3
Michael Chris Lopez <hello@michaelchris.space>
2024-02-10 13:58:14 +0100

    VsSnitch to open file in vscode

--- vim/Library/Preferences/nvim/after/plugin/opt.lua
+++ vim/Library/Preferences/nvim/after/plugin/opt.lua
@@ -6,5 +6,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.api.nvim_create_user_command("VsSnitch", function(opts)
+	local cmd = { "code" }
+	if opts.args == "" then
+		local fname = vim.api.nvim_buf_get_name(0)
+		local cpos = vim.api.nvim_win_get_cursor(0)
+		local fnamecol = fname .. ":" .. cpos[1] .. ":" .. (cpos[2] + 1)
+		cmd = vim.list_extend(cmd, { "-g", fnamecol })
+	else
+		cmd = vim.list_extend(cmd, opts.fargs)
+	end
+	vim.fn.jobstart(cmd)
+end, { nargs = "*", complete = "file" })
+
 -- Common typo
 vim.cmd.cnoreabbrev "w; w"
