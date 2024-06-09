1dae52022f879d1485a11293f3f351595976286d
Michael Chris Lopez <hello@michaelchris.space>
2024-06-09 14:01:42 +0200

    use vim.keymap

--- vim/Library/Preferences/nvim/after/plugin/keys.lua
+++ vim/Library/Preferences/nvim/after/plugin/keys.lua
@@ -1,20 +1,6 @@
-local wk = require "which-key"
-
-wk.register {
-	["<leader>"] = {
-		["<cr>"] = {
-			function()
-				vim.cmd.nohlsearch()
-				vim.cmd.checktime()
-				vim.cmd.redraw()
-				print "Refreshed!"
-			end,
-			"Refresh",
-		},
-	},
-}
-
--- wk.register({
--- 	["j"] = { "v:count == 0 ? 'gj' : 'j'" },
--- 	["k"] = { "v:count == 0 ? 'gk' : 'k'" },
--- }, { mode = { "n", "x" }, expr = true, silent = true, noremap = false })
+vim.keymap("n", "<leader><cr>", function()
+	vim.cmd.nohlsearch()
+	vim.cmd.checktime()
+	vim.cmd.redraw()
+	print "Refreshed!"
+end, { desc = "Refresh" })
