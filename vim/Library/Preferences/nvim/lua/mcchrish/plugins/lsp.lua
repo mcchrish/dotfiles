3642b103d84f87d032024ce5408a0f902e6b10f3
Michael Chris Lopez <hello@michaelchris.space>
2024-06-09 14:01:11 +0200

    update lsp

--- vim/Library/Preferences/nvim/lua/mcchrish/plugins/lsp.lua
+++ vim/Library/Preferences/nvim/lua/mcchrish/plugins/lsp.lua
@@ -92,7 +92,6 @@ return {
 		config = function(_, opts)
 			vim.diagnostic.config(opts.diagnostics)
 
-			local wk = require "which-key"
 			local lspconfig = require "lspconfig"
 			require("mason-lspconfig").setup {
 				ensure_installed = { "eslint", "lua_ls", "emmet_language_server", "vale_ls" },
@@ -144,28 +143,23 @@ return {
 				on_attach = function(client, bufnr)
 					client.server_capabilities.documentFormattingProvider = false
 					client.server_capabilities.documentRangeFormattingProvider = false
-					wk.register({
-						["<leader>gr"] = {
-							function()
-								client.request(
-									"volar/client/findFileReference",
-									{ textDocument = vim.lsp.util.make_text_document_params(bufnr) },
-									function(_, locations, context)
-										local items = vim.lsp.util.locations_to_items(locations, client.offset_encoding)
-										vim.fn.setqflist(
-											{},
-											" ",
-											{ title = "Vue File References", items = items, context = context }
-										)
-										require("fzf-lua").quickfix()
-										-- vim.api.nvim_command "copen"
-									end,
-									bufnr
+					vim.keymap.set("n", "<leader>gr", function()
+						client.request(
+							"volar/client/findFileReference",
+							{ textDocument = vim.lsp.util.make_text_document_params(bufnr) },
+							function(_, locations, context)
+								local items = vim.lsp.util.locations_to_items(locations, client.offset_encoding)
+								vim.fn.setqflist(
+									{},
+									" ",
+									{ title = "Vue File References", items = items, context = context }
 								)
+								require("fzf-lua").quickfix()
+								-- vim.api.nvim_command "copen"
 							end,
-							"Vue file references",
-						},
-					}, { mode = "n", noremap = true, buffer = bufnr })
+							bufnr
+						)
+					end, { buffer = bufnr, desc = "Vue file references" })
 				end,
 			}
 
@@ -175,27 +169,20 @@ return {
 
 			lspconfig.emmet_language_server.setup {
 				on_attach = function(client, bufnr)
-					wk.register({
-						["<c-s>"] = {
-							[","] = {
-								function()
-									client.request(
-										"textDocument/completion",
-										vim.lsp.util.make_position_params(0, client.offset_encoding),
-										function(_, result)
-											local textEdit = result.items[1].textEdit
-											local snip_string = textEdit.newText
-											textEdit.newText = ""
-											vim.lsp.util.apply_text_edits({ textEdit }, bufnr, client.offset_encoding)
-											require("luasnip").lsp_expand(snip_string)
-										end,
-										bufnr
-									)
-								end,
-								"Expand emmet",
-							},
-						},
-					}, { mode = "i", noremap = true, buffer = bufnr })
+					vim.keymap.set("i", "<c-s>,", function()
+						client.request(
+							"textDocument/completion",
+							vim.lsp.util.make_position_params(0, client.offset_encoding),
+							function(_, result)
+								local textEdit = result.items[1].textEdit
+								local snip_string = textEdit.newText
+								textEdit.newText = ""
+								vim.lsp.util.apply_text_edits({ textEdit }, bufnr, client.offset_encoding)
+								require("luasnip").lsp_expand(snip_string)
+							end,
+							bufnr
+						)
+					end, { noremap = true, buffer = bufnr, desc = "Expand emmet" })
 				end,
 			}
 
@@ -222,56 +209,62 @@ return {
 
 			lspconfig.vale_ls.setup { capabilities = capabilities }
 
-			wk.register({
-				["<leader>"] = {
-					e = {
-						vim.diagnostic.open_float,
-						"Line diagnostics",
-					},
-					q = { vim.diagnostic.setloclist, "Open diagnostics location list" },
-				},
-			}, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("lsp-attach", {}),
 				callback = function(event)
 					-- Enable completion triggered by <c-x><c-o>
 					vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-					wk.register({
-						g = {
-							D = { require("fzf-lua").lsp_declarations, "Declaration" },
-							d = { require("fzf-lua").lsp_definitions, "Definition" },
-							i = { require("fzf-lua").lsp_implementations, "Implementation" },
-							rr = { require("fzf-lua").lsp_references, "References" },
-							rn = { vim.lsp.buf.rename, "Rename" },
-							ca = { require("fzf-lua").lsp_code_actions, "Code action" },
-							K = { vim.lsp.buf.signature_help, "Signature Help" },
-							["<c-d>"] = { require("fzf-lua").lsp_typedefs, "Type definition" },
-						},
-						["<leader>w"] = {
-							name = "+LSP workspace",
-							a = { vim.lsp.buf.add_workspace_folder, "add folder" },
-							r = { vim.lsp.buf.remove_workspace_folder, "remove folder" },
-							l = {
-								function()
-									print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-								end,
-								"list folders",
-							},
-						},
-					}, { noremap = true, buffer = event.buf })
+
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					local fzf = require "fzf-lua"
+
+					map("gd", fzf.lsp_definitions, "[G]oto [D]efinition")
+					map("gr", fzf.lsp_references, "[G]oto [R]eferences")
+					map("gI", fzf.lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>D", fzf.lsp_typedefs, "Type [D]efinition")
+					map("<leader>ds", fzf.lsp_document_symbols, "[D]ocument [S]ymbols")
+					map("<leader>ws", fzf.lsp_live_workspace_symbols, "[W]orkspace [S]ymbols")
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>ca", fzf.lsp_code_actions, "[C]ode [A]ction")
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client and client.server_capabilities.documentHighlightProvider then
+						local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 							buffer = event.buf,
+							group = highlight_augroup,
 							callback = vim.lsp.buf.document_highlight,
 						})
 
 						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 							buffer = event.buf,
+							group = highlight_augroup,
 							callback = vim.lsp.buf.clear_references,
 						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds { group = "lsp-highlight", buffer = event2.buf }
+							end,
+						})
+					end
+
+					-- The following autocommand is used to enable inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
+					if client and client.server_capabilities.inlayHintProvider then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+						end, "[T]oggle Inlay [H]ints")
 					end
 				end,
 			})
