c29db742deac269768e8657e090de02104669bde
Michael Chris Lopez <hello@michaelchris.space>
2024-08-03 20:43:56 +0200

    update init.lua

--- vim/Library/Preferences/nvim/init.lua
+++ vim/Library/Preferences/nvim/init.lua
@@ -6,7 +6,7 @@ g.did_install_default_menus = 1
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system {
+	vim.system {
 		"git",
 		"clone",
 		"--filter=blob:none",
