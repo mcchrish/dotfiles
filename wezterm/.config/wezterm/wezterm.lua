06a0346074560294c6e84c9a3be40fbf6a387920
Michael Chris Lopez <hello@michaelchris.space>
2024-08-15 08:19:01 +0200

    wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,14 +5,15 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "zenbones"
 -- https://github.com/wez/wezterm/issues/3616 font looks too thin
-config.font = wezterm.font "Iosevka Curly Slab"
-config.font_size = 14
+config.font = wezterm.font { family = "Zenbones Mono", harfbuzz_features = { "TXTR", "cv07=10", "cv10=13", "VSAG=1" } }
+config.font_size = 13
+config.line_height = 1.04
 config.initial_cols = 160
 config.initial_rows = 40
 -- config.front_end = "WebGpu"
 -- config.automatically_reload_config = false
 -- config.freetype_load_target = "Light"
--- config.freetype_load_flags = "NO_HINTING"
+config.freetype_load_flags = "NO_HINTING"
 -- config.freetype_render_target = "HorizontalLcd"
 
 config.keys = {
