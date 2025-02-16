6e0a0553a750877e731ad3bdd37f53b9a2e9255c
Michael Chris Lopez <hello@michaelchris.space>
2025-02-16 18:59:15 +0100

    cmp style

--- vim/Library/Preferences/nvim/lua/mcchrish/plugins/completion.lua
+++ vim/Library/Preferences/nvim/lua/mcchrish/plugins/completion.lua
@@ -1,23 +1,11 @@
 return {
 	{
 		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
 		dependencies = "rafamadriz/friendly-snippets",
-
-		-- use a release tag to download pre-built binaries
 		version = "*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
-
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
 			keymap = {
 				preset = "default",
 
@@ -25,6 +13,20 @@ return {
 
 				["<c-l>"] = { "snippet_forward", "fallback" },
 				["<c-h>"] = { "snippet_backward", "fallback" },
+			},
+
+			cmdline = {
+				keymap = {
+					preset = "super-tab",
+				},
+			},
+
+			completion = {
+				menu = {
+					draw = {
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 2 } },
+					},
+				},
 			},
 
 			appearance = {
